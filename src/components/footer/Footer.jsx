8a5948c15e91a6360d85363ce7eef876a8b2d343
reececilon <reecethat@gmail.com>
2022-10-27 16:38:00 +0100

    Fixed image sizing and changed CV and removed services link from footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -23,7 +23,7 @@ function Footer() {
                 <li><a href='#'>Home</a></li>
                 <li><a href='#about'>About</a></li>
                 <li><a href='#experience'>Experience</a></li>
-                <li><a href='#services'>Services</a></li>
+                {/* <li><a href='#services'>Services</a></li> */}
                 <li><a href='#portfolio'>Portfolio</a></li>
                 <li><a href='#testimonial'>Testimonials</a></li>
                 <li><a href='#contact'>Contact</a></li>
